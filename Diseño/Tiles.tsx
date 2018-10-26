f61425f5077f3d17cf4a4ca191c4719ee6a87f30
Javier <javier.albar.98@hotmail.com>
2018-10-26 11:44:12 +0200

    Archivos del mapa de tiles de los distintos niveles

---
+++ Diseño/Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Tiles" tilewidth="48" tileheight="48" tilecount="117" columns="13">
+ <image source="D:/JRPráctica/Völva Chronicle/assets/platforms/tiles/Tiles.png" width="624" height="432"/>
+</tileset>
