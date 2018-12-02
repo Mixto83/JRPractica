37f45547abf0fcb9a70a810e83d30f9d3ec82b92
AlbaranezJavier <javier.albar.98@hotmail.com>
2018-12-02 18:43:26 +0100

    csv para la vegetación el la capa que queda detrás del juegador y el conjunto de patrones utilizados en el programa Tiled.

---
+++ Diseño/vegetacion.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="vegetacion" tilewidth="48" tileheight="48" tilecount="27" columns="9">
+ <image source="../VolvaChronicles/src/main/resources/static/assets/platforms/tiles/vegetacion.png" width="432" height="144"/>
+</tileset>
